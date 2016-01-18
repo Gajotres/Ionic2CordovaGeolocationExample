4498621902c8c0d0d7b94ca7748be4e0ee05fde0
Dragan Gaic <dragan.gaic@outlook.com>
2016-01-18 15:27:00 +0100

    First Commit

---
+++ app/pages/mapPage/mapPage.js
@@ -0,0 +1,42 @@
+import {Page, Platform} from 'ionic/ionic';
+
+@Page({
+  templateUrl: 'build/pages/mapPage/mapPage.html',
+})
+
+export class MapPage {
+	
+	constructor(platform: Platform) {
+		this.platform = platform;
+
+		this.initializeMap();    
+	}
+
+	initializeMap() {
+
+		this.platform.ready().then(() => {
+
+			let locationOptions = {timeout: 10000, enableHighAccuracy: true};
+
+			navigator.geolocation.getCurrentPosition(
+
+				(position) => {
+
+					let options = {
+					  center: new google.maps.LatLng(position.coords.latitude, position.coords.longitude),
+					  zoom: 16,
+					  mapTypeId: google.maps.MapTypeId.ROADMAP
+					}
+
+					this.map = new google.maps.Map(document.getElementById("map_canvas"), options);
+				},
+
+				(error) => {
+					console.log(error);
+				}, locationOptions
+
+			);
+
+		});
+	}  
+}
